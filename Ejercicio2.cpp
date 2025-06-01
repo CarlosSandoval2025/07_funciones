f56364237f76b73b427e9bf52b7b4d82a2e69e86
CarlosSandoval2025 <carlos.j98232s.s@gmail.com>
2025-05-31 19:21:24 -0500

    Ejercicio2: factorial

---
+++ Ejercicio2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int);
+
+int main(){
+	int m,n,c;
+	cin>>m>>n;
+	c=factorial(m)/(factorial(n)*factorial(m-n));
+	cout<<c; 
+	return 0; 
+}
+int factorial(int n){
+	int f; 
+	for (int i=2;i<=n;i++)
+		f=f*i;
+	return f;
+}
+
