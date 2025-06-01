60d73f9bfed2615d2b3144430c621847517f2b0c
CarlosSandoval2025 <carlos.j98232s.s@gmail.com>
2025-05-31 19:37:19 -0500

    Actualizacion Ejercicio2: se agrego una condición para que no ubiece divición entre cero

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -6,12 +6,16 @@ int factorial(int);
 int main(){
 	int m,n,c;
 	cin>>m>>n;
+		if(n==0||(m-n)==0){
+		cout<<"El factorial no existe ";
+	}
 	c=factorial(m)/(factorial(n)*factorial(m-n));
-	cout<<c; 
-	return 0; 
+	cout<<"La combinatoria es: "<<c<<endl; 
+    return 0; 
 }
 int factorial(int n){
 	int f; 
+	f=1;
 	for (int i=2;i<=n;i++)
 		f=f*i;
 	return f;
